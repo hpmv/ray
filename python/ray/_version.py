588c828858d278edf4689477478e58bc15625001
hpmv <hpmv>
2025-11-22 08:02:55 +0000

    Wheels?

--- python/ray/_version.py
+++ python/ray/_version.py
@@ -1,5 +1,5 @@
 # Replaced with the current commit when building the wheels.
-commit = "{{RAY_COMMIT_SHA}}"
+commit = "65af0b533d04b2cab3985c3c1f865f607718e5a4"
 version = "2.51.1"
 
 if __name__ == "__main__":
